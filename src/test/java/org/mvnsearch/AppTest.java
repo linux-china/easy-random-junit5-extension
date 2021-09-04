04be3945f28ca9d40626cbbc0785b4d4e0c0308e
linux_china <libing.chen@gmail.com>
2021-09-04 16:44:30 -0700

    AppTest

---
+++ src/test/java/org/mvnsearch/AppTest.java
@@ -0,0 +1,36 @@
+package org.mvnsearch;
+
+
+import org.jeasy.random.EasyRandomExtension;
+import org.jeasy.random.Random;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(EasyRandomExtension.class)
+public class AppTest {
+    @Random
+    @Email
+    private String email;
+
+    @Test
+    public void shouldAnswerWithTrue(@Random @Positive int value) {
+        assertThat(value).isGreaterThan(0);
+        assertThat(email).contains("@");
+    }
+
+    @Test
+    public void testRandomUser(@Random User user) {
+        assertThat(user.getEmail()).contains("@");
+    }
+
+    @Test
+    public void testRandomUserList(@Random(size = 5) List<User> users) {
+        assertThat(users).hasSize(5);
+    }
+}

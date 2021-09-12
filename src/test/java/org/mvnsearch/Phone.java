bd5a9e37e5220da2e536c56ec17be9cef1bd5899
linux_china <libing.chen@gmail.com>
2021-09-12 13:54:49 -0700

    Phone annotation

---
+++ src/test/java/org/mvnsearch/Phone.java
@@ -0,0 +1,18 @@
+package org.mvnsearch;
+
+import javax.validation.Constraint;
+import javax.validation.constraints.Pattern;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Constraint(validatedBy = {})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Pattern(regexp = "(136|137|186)\\d{8}")
+public @interface Phone {
+}

e8543468b737a22e156471a09158505531a32117
linux_china <libing.chen@gmail.com>
2021-10-19 13:35:47 -0700

    Polishing

--- src/test/java/org/mvnsearch/RecordTest.java
+++ src/test/java/org/mvnsearch/RecordTest.java
@@ -6,16 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Positive;
 
 @ExtendWith(EasyRandomExtension.class)
 public class RecordTest {
 
-    record Person(int id, @Email String name) {
+    record Person(@Positive int id, @Email String email) {
     }
 
     @Test
     public void testRecord(@Random Person person) {
         System.out.println("person.id = " + person.id());
-        System.out.println("person.name = " + person.name());
+        System.out.println("person.email = " + person.email());
     }
 }

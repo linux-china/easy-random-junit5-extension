0f4de2e599d8f9e4693ac2dcc8d407213ee5662f
linux_china <libing.chen@gmail.com>
2022-11-01 13:18:49 +0800

    test: Account

---
+++ src/test/java/org/mvnsearch/Account.java
@@ -0,0 +1,22 @@
+package org.mvnsearch;
+
+public class Account {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
